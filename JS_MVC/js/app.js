fa348bc2898815a0f16cb2237438da8297c5a50f
simjongyeon3 <simjongyeon3@gmail.com>
2023-08-14 09:34:52 +0900

    20230814 3st commit

---
+++ JS_MVC/js/app.js
@@ -0,0 +1,8 @@
+import MainController from "./controller/MainController.js";
+
+// DOM이 완성이 되면, MainController 를 가져와서 초기화하도록 함.
+document.addEventListener(
+  "DOMContentLoaded", () => {
+    //MainController.init()
+  }
+);

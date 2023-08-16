0d087f146e05f9d0acf50ad0b1b226ac1312b2cf
simjongyeon3 <simjongyeon3@gmail.com>
2023-08-16 15:34:02 +0900

    20230816 7st commit

--- JS_MVC/js/views/HistoryView.js
+++ JS_MVC/js/views/HistoryView.js
@@ -38,10 +38,34 @@ HistoryView.getKeywordsHtml = function (data) {
  * 버튼 이벤트(자식) => li 요소의 이벤트(부모) : 버블링 이벤트
  * 버블링 이벤트가 발생하지 않도록 제어가 필요함
  */
+HistoryView.bindRemoveBtn = function () {
+  Array.from(this.el.querySelectorAll("button.btn-remove"))
+    .forEach( btn => {
+                        btn.addEvnetListener( "click", e => {
+                            e.stopPropagation() // 이벤트 전파 멈춤.
+
+                            /***
+                             * MainController 에서 사용할 수 있도록
+                             * 사용자 이벤드 생성 함수 호출
+                             * 
+                             * 매개변수 : 부모의 값( 검색 키워드 이력 )
+                             */
+                            this.onRemove(btn.parentElement.dataset.keyword)
+                          }
+                        ) // end of addEvnetListener
+      } // end of btn callback method
+    ) // end of forEach
+}
 
 
-//"X"버튼, 삭제 버튼에 대한 사용자 이벤트 생성 의뢰 함수
 
+/***
+ * "X"버튼, 삭제 버튼에 대한 사용자 이벤트 생성 의뢰 함수
+ * keyword : 부모 요소의 값( 검색 키워드 )
+ */
+HistoryView.onRemove = function (keyword) {
+  this.dispatch("@remove", {keyword})
+}
 
 
 export default HistoryView

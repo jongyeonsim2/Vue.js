2a223e608721da2c32c37319148ece2e18140c7f
simjongyeon3 <simjongyeon3@gmail.com>
2023-08-21 10:02:45 +0900

    20230821 1st commit

---
+++ vue-todo-my/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <!-- 컴포넌트 태그 4가지를 작성 -->
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+// 화면 요소 4개의 모듈을 사용하기 위해서 import 함.
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+export default {
+  components: {
+    // 컴포넌트 태그와 실제 모듈의 관계를 설정.
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #F6F6F8;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+}
+</style>
---
+++ vue-todo-my/src/components/TodoFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>footer</div>
+</template>
+
+<script></script>
+
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
+</style>
